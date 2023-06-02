f2c67bd0603289b14ec7dbf1dad3631ca2bf3b4e
Kanykei Tashbaeva <tashbaevakanykei@gmail.com>
2023-06-02 19:03:42 +0200

    fix: header in php

--- var/www/pages/cgi-bin/post.php
+++ var/www/pages/cgi-bin/post.php
@@ -21,7 +21,7 @@ $requestBody = file_get_contents("php://input");
 			$response = file_get_contents($responseFile);
 			$message = "✅ Data saved successfully to ";
 			$message .= $directory . "\r\n";
-			http_response_code(201);
+			http_response_code(200);
 		} else {
 			http_response_code(500);
 			$message = "❌ Failed to load response template";
@@ -39,11 +39,20 @@ $response = preg_replace('/{{message}}/i', $message, $response);
 $date = gmdate('D, d M Y H:i:s T');
 $server = getenv('SERVER_NAME');
 
+header("HTTP/1.1" . http_response_code() . " " . http_response_code_message(http_response_code()));
 header("Content-Type: text/html; charset=UTF-8");
 header("Content-Length: " . strlen($response));
 header("Connection: close");
 header("Date: " . $date);
 header("Server: " . $server);
 echo $response;
+
+function http_response_code_message($code) {
+	$messages = array(
+		200 => 'OK',
+		500 => 'Internal Server Error',
+	);
+	return isset($messages[$code]) ? $messages[$code] : '';
+}
 
 ?>
